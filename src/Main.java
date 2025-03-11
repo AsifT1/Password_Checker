8d69bbb7df34905d62dfa7c2926c8f894b9aee49
AsifT1 <AsifT1@umbc.edu>
2025-03-10 21:02:37 -0400

    Added code

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter your password: ");
+        System.out.print("Enter your passwords: ");
         String password = scanner.nextLine();
 
 
